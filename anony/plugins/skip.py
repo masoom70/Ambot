6167ca92d9266f9fcecb3c4f774c7df13062774f
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2026-04-29 03:11:02 +0530

    Implement try-except for reply in skip command
    Add error handling for reply in skip command

--- anony/plugins/skip.py
+++ anony/plugins/skip.py
@@ -1,13 +1,6 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
 from pyrogram import filters, types
-
 from anony import anon, app, db, lang
 from anony.helpers import can_manage_vc
-
 
 @app.on_message(filters.command(["skip", "next"]) & filters.group & ~app.bl_users)
 @lang.language()
@@ -17,4 +10,10 @@ async def _skip(_, m: types.Message):
         return await m.reply_text(m.lang["not_playing"])
 
     await anon.play_next(m.chat.id)
-    await m.reply_text(m.lang["play_skipped"].format(m.from_user.mention))
+    
+    try:
+        await m.reply_text(
+            m.lang["play_skipped"].format(m.from_user.mention)
+        )
+    except Exception:
+        pass
